540214633d8e49dbb7e39eeb37287c766ba8df82
deskach <dzianis.kachanau@gmail.com>
2016-06-25 09:22:48 +1000

    Implemented templated inheritance

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -12,5 +12,9 @@ def index():
                            user=User("Siarhei", "Kachanovich")
                            )
 
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
 if __name__ == '__main__':
     app.run(debug=True) 
