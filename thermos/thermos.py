71b246e209172ba8acda46daef756544ac2844bc
deskach <dzianis.kachanau@gmail.com>
2016-06-25 09:38:46 +1000

    Added server-side error handling

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -12,6 +12,14 @@ def index():
                            user=User("Siarhei", "Kachanovich")
                            )
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return (render_template('404.html'), 404)
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return (render_template('500.html'), 500)
+
 @app.route('/add')
 def add():
     return render_template('add.html')
