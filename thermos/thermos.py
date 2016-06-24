df3d1ea58e9e2c64b5e40efa1df9666a937dac6b
deskach <dzianis.kachanau@gmail.com>
2016-06-25 03:16:16 +1000

    added class User and used it in the view

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from models.user import User
 
 app = Flask(__name__)
 
@@ -6,7 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', 
+                           title = "Thermos",
+                           user=User("Siarhei", "Kachanovich")
+                           )
 
 if __name__ == '__main__':
     app.run(debug=True) 
