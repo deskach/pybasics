384bdb06009b3fa5dcd1ac7887caeca84a29c22d
deskach <dzianis.kachanau@gmail.com>
2016-06-25 10:52:32 +1000

    Implemented POST request to store bookmarks

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from datetime import datetime
 from models.user import User
+from logging import DEBUG
 
 app = Flask(__name__)
+app.logger.setLevel(DEBUG)
 
+bookmarks = []
 
 @app.route('/')
 @app.route('/index')
@@ -20,8 +24,22 @@ def page_not_found(e):
 def page_not_found(e):
     return (render_template('500.html'), 500)
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add():
+    def store_bookmark(url):
+        bookmarks.append({
+            "url": url,
+            "user": "reindert",
+            "date": datetime.utcnow()
+        })
+
+    if request.method == "POST":
+        url = request.form['url'] # url here is the input's name'
+        store_bookmark(url)
+        app.logger.debug('stored url: ' + url)
+
+        return redirect(url_for('index'))
+    
     return render_template('add.html')
 
 if __name__ == '__main__':
