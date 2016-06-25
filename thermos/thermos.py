3b3c66ee1c3e29e46add4968c911e9de2b529720
deskach <dzianis.kachanau@gmail.com>
2016-06-25 16:49:03 +1000

    Now thermos relies on the database to handle bookmarks

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,25 +1,30 @@
+import os
 from flask import Flask, render_template, url_for, request, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from models.user import User
 from logging import DEBUG
 from forms import BookmarkForm
+import models
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.logger.setLevel(DEBUG)
 app.config['SECRET_KEY'] = '>\x8e\xaf}\x11`n\xd62\x8a\x98>a\x0b\xe1\x1d-V\x00\xa0^\xff[\xcb\xaa'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)    # this is imported in models.py 
 
 bookmarks = []
 
 @app.route('/')
 @app.route('/index')
 def index():
-    def new_bookmarks(num):
-        return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
+    # def new_bookmarks(num):
+        # return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
 
     return render_template('index.html', 
                            title = "Thermos",
-                           user=User("Siarhei", "Kachanovich"),
-                           new_bookmarks=new_bookmarks(5)
+                           new_bookmarks=models.Bookmark.new_bookmarks(5)
                            )
 
 @app.errorhandler(404)
@@ -31,20 +36,14 @@ def page_not_found(e):
     return (render_template('500.html'), 500)
 
 @app.route('/add', methods=['GET', 'POST'])
-def add():
-    def store_bookmark(url, description=""):
-        bookmarks.append({
-            "url": url,
-            "description": description,
-            "user": "reindert",
-            "date": datetime.utcnow()
-        })
-    
+def add():    
     form = BookmarkForm()
     if form.validate_on_submit():
         url = form.url.data
         description = form.description.data
-        store_bookmark(url, description)
+        bm = models.Bookmark(url=url, description=description)
+        db.session.add(bm)
+        db.session.commit()
         flash("stored url: '%s'" % url)
 
         return redirect(url_for('index'))    
