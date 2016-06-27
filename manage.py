f71a268218b503d49f45bd9d53a76816e32d8084
deskach <dzianis.kachanau@gmail.com>
2016-06-27 11:58:49 +1000

    Made use of flask_migrate

--- manage.py
+++ manage.py
@@ -1,22 +1,42 @@
 #! /uar/bin/env python
 
-from flask_script import Manager, prompt_bool
-
 from thermos import app, db
-from models import User
+from models import User, Bookmark, Tag
+
+from flask_script import Manager, prompt_bool
+from flask_migrate import Migrate, MigrateCommand
 
 manager = Manager(app)
+migrate = Migrate(app, db)
+
+manager.add_command('db', MigrateCommand)
 
 
 @manager.command
-def initdb():
-    ''' Initializes the database'''
-    db.create_all()
-    db.session.add(User(username='tolia', email="tolia@test.com", password="test"))
-    db.session.add(User(username='daryna', email="daryna@gmail.com", password="test"))
+def insert_data():
+    reindert = User(username="reindert", email="reindert@example.com", password="test")
+    db.session.add(reindert)
+
+    def add_bookmark(url, description, tags):
+        db.session.add(Bookmark(url=url, description=description, user=reindert,
+                                tags=tags))
+
+    for name in ["python", "flask", "webdev", "programming", "training", "news", "orm", "databases", "emacs", "gtd",
+                 "django"]:
+        db.session.add(Tag(name=name))
     db.session.commit()
 
-    print('Initialized the database')
+    add_bookmark("http://www.pluralsight.com", "Pluralsight. Hardcore developer training.",
+                 "training,programming,python,flask,webdev")
+    add_bookmark("http://www.python.org", "Python - my favorite language", "python")
+    add_bookmark("http://flask.pocoo.org", "Flask: Web development one drop at a time.", "python,flask,webdev")
+    add_bookmark("http://www.reddit.com", "Reddit. Frontpage of the internet", "news,coolstuff,fun")
+    add_bookmark("http://www.sqlalchemyorg", "Nice ORM framework", "python,orm,databases")
+
+    arjen = User(username="arjen", email="arjen@robben.nl", password="test")
+    db.session.add(arjen)
+    db.session.commit()
+    print ('Initialized the database')
 
 
 @manager.command
@@ -24,12 +44,6 @@ def dropdb():
     '''Drops the database'''
     db.drop_all()
     print('Dropped the database')
-
-
-@manager.command
-def recreatedb():
-    dropdb()
-    initdb()
 
 
 @manager.command
